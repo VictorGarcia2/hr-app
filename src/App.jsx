370b388924522885a74f2daa8cb474e2e4ec80d0
VictorGarcia2 <victrgarciaphoto@gmail.com>
2025-02-11 10:34:40 -0600

    Agregar componentes Drawer y Login a la aplicación

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,11 @@
-
+import { Drawer } from "./components/Drawer";
+import Login from "./components/Login";
 
 export default function App() {
   return (
-
-  )
+    <>
+      <Drawer />
+      <Login />
+    </>
+  );
 }
