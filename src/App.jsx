e61aae926a8e1e88736662cd096c1c6c0676b958
Luis Farias <popshovit33@gmail.com>
2025-02-11 13:14:37 -0600

    services table created

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import Login from './components/Login'
+import ServiciosDeEstudiantes from './components/Servicios-de-estudiantes'
 
 export default function App() {
   return (
