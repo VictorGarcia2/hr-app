84d50dae2e3e21f21a8abd6cf8135012203d84dd
Moises Cruz <serviciopatrimonial@gmail.com>
2025-02-11 14:09:31 -0600

    commit instance

---
+++ src/libs/axios/instance.js
@@ -0,0 +1,21 @@
+import axios from 'axios'
+
+export default function instance() {
+  
+    const instance = axios.create({
+        baseURL: 'https://www.hs-service.api.crealape.com/api/v1',
+        withCredentials: true
+    });
+    
+    instance.interceptors.response.use((response) => response, (error)=>{
+        if(error.status === 401){
+            window.location.href = "/login"
+        }
+        if(error.status === 403){
+            windows.location.href = "/"
+        }
+        return Promise.reject(error);
+    })
+    
+  
+}
