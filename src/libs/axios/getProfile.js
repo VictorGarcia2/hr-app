aa8d4a92bd523c0c5ccd9618781c1b19cc133361
Gustavo Lastra <lastraemiliano1@gmail.com>
2025-02-12 14:48:12 -0600

    agregar estudiantes a la tabla, filtrando la data

--- src/libs/axios/getProfile.js
+++ src/libs/axios/getProfile.js
@@ -3,6 +3,7 @@ import instance from "./instance"
 export async function getProfile() {
     try{
         const { status, data} = await instance.get('/auth/profile')
+        
         return { status, data}    
     } catch (error){
         console.error(error)
